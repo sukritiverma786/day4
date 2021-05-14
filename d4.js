a660e0be04373586bb1b5cc64ede15cb1212d48f
sukriti <sukritiverma786@gmail.com>
2021-05-14 12:59:52 +0530

    my foruth day solution commit

---
+++ d4.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+// sum of three number
+var a = 1;
+var b = 2;
+var c = 3;
+var add = a + b + c;
+console.log("the sum of three digit " + a + " , " + b + " and " + c + " is");
+console.log(add);
+// divide by 2
+console.log();
+var x = 1;
+var y = 2;
+var z = 3;
+var divide = (x + y + z) / 2;
+console.log("divide ans is " + x + " and " + y + " and " + z);
+console.log(divide);
